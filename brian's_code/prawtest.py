1e8972b40655a98e4d2ab4e4e91d100caf860fdd
tofuulery <95109887+tofuulery@users.noreply.github.com>
2022-02-14 15:34:31 -0600

    added **kwargs for limiting get_submissions in prawtest.py.

--- brian's_code/prawtest.py
+++ brian's_code/prawtest.py
@@ -6,12 +6,16 @@ reddit = praw.Reddit(
     client_secret="DEOTHnpheAYiMjAEklkPmXYLT6141A",
     user_agent="prawtest")
 
-def get_submissions(query, sub):
+def get_submissions(query, sub, **kwarg):
+    if kwarg.get('limit'):
+        limit = kwarg.get('limit')
+    else:
+        limit = None
     subreddit = reddit.subreddit(f'{sub}')
-    df = pd.DataFrame([vars(post) for post in subreddit.search(f'{query}', limit = None) ])
+    df = pd.DataFrame([vars(post) for post in subreddit.search(f'{query}', limit = limit) ])
     query2 = query.replace('|','OR')
     query3 = query2.replace('+','AND')
     query4 = query3.replace(' ', '-')
     df.to_csv(f'../data/praw_q={query4}_sub={sub}.csv')
 
-get_submissions('regulation', 'delta8')
+get_submissions('regulation', 'delta8', limit=None)
