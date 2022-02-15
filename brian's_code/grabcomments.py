6bec7aac3489a098959f2fbf6793316cabdf578d
tofuulery <95109887+tofuulery@users.noreply.github.com>
2022-02-14 19:25:21 -0600

    error handling

--- brian's_code/grabcomments.py
+++ brian's_code/grabcomments.py
@@ -7,15 +7,18 @@ rh = reddithelper()
 
 datapath = '../data'
 os.chdir(datapath)
-
-for file in os.listdir(datapath):
+files = [f for f in os.listdir('.') if os.path.isfile(f)]
+for file in files:
     if 'praw' in file:
         df = pd.read_csv(file)
-        os.mkdir('comments_'+str(file.strip('.csv')))
-        # os.chdir('comments_'+str(file.strip('.csv')))
-        for id in tqdm(df.id):
-            try:
-                df_post = rh.grab_comments_praw(id)
-                df_post.to_csv('comments_'+file.strip('.csv') + '/' + id + '.csv')
-            except AttributeError:
-                pass
+        if os.path.isdir('comments_'+str(file.strip('.csv'))):
+            pass
+        else:
+            os.mkdir('comments_'+str(file.strip('.csv')))
+            # os.chdir('comments_'+str(file.strip('.csv')))
+            for id in tqdm(df.id):
+                try:
+                    df_post = rh.grab_comments_praw(id)
+                    df_post.to_csv('comments_'+file.strip('.csv') + '/' + id + '.csv')
+                except AttributeError:
+                    pass
--- brian's_code/prawtest.py
+++ brian's_code/prawtest.py
@@ -1,5 +1,7 @@
 import praw
 import pandas as pd
+from tqdm import tqdm
+import os
 
 reddit = praw.Reddit(
     client_id="B9p-21QJHz9QYsiiY6bFpw",
@@ -7,22 +9,30 @@ reddit = praw.Reddit(
     user_agent="prawtest")
 
 def get_submissions(query, sub, **kwarg):
+    query2 = query.replace('|','OR')
+    query3 = query2.replace('+','AND')
+    query4 = query3.replace(' ', '-')
+    filename = f'../data/praw_q={query4}_subreddit={sub}.csv'
     if kwarg.get('limit'):
         limit = kwarg.get('limit')
     else:
         limit = None
     subreddit = reddit.subreddit(f'{sub}')
-    df = pd.DataFrame([vars(post) for post in subreddit.search(f'{query}', limit = limit) ])
-    query2 = query.replace('|','OR')
-    query3 = query2.replace('+','AND')
-    query4 = query3.replace(' ', '-')
-    df.to_csv(f'../data/praw_q={query4}_subreddit={sub}.csv')
+    if os.path.exists(filename):
+        pass
+    else:
+        df = pd.DataFrame([vars(post) for post in subreddit.search(f'{query}', limit = limit) ])
+        if 'id' in df.keys():
+            df.to_csv(filename)
+        else:
+            pass
 
-limit = 1000
 
-sublist = ['delta8', 'delta8testing']
-qlist = ['regulat+report+law]
+if __name__ == '__main__':
+    limit = 1000
+    sublist = ['delta8', 'delta8testing']
+    qlist = ['law|legislation|legislator|government']
 
-for sub in sublist:
-    for q in qlist:
-        get_submissions(q, sub, limit=limit)
+    for sub in tqdm(sublist, desc=f'Subreddit = {sub}'):
+        for q in tqdm(qlist, desc = f'Query = {q}'):
+            get_submissions(q, sub, limit=limit)
