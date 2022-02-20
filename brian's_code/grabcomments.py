78c122d5dd4c761f0401ac0e1bad7a2cb918be3a
TheFermiSea <56853024+TheFermiSea@users.noreply.github.com>
2022-02-19 22:07:36 -0600

    reorganized data

--- brian's_code/grabcomments.py
+++ brian's_code/grabcomments.py
@@ -5,6 +5,20 @@ from tqdm import tqdm
 rh = reddithelper()
 
 
+def reorder_columns(dataframe, col_name, position):
+    """Reorder a dataframe's column.
+    Args:
+        dataframe (pd.DataFrame): dataframe to use
+        col_name (string): column name to move
+        position (0-indexed position): where to relocate column to
+    Returns:
+        pd.DataFrame: re-assigned dataframe
+    """
+    temp_col = dataframe[col_name]
+    dataframe = dataframe.drop(columns=[col_name])
+    dataframe.insert(loc=position, column=col_name, value=temp_col)
+    return dataframe
+
 datapath = '../data'
 os.chdir(datapath)
 files = [f for f in os.listdir('.') if os.path.isfile(f)]
@@ -19,6 +33,10 @@ for file in files:
             for id in tqdm(df.id, desc=f'{id}'):
                 try:
                     df_post = rh.grab_comments_praw(id)
+                    columns_list = ['id', 'author', 'title', 'body', 'created','author_flair_text', 'link_flair_text']
+                    for i,j in enumerate(columns_list):
+                        df = reorder_columns(dataframe=df, col_name= j, position=i)
+                        print('csv created')
                     df_post.to_csv('comments_'+file.strip('.csv') + '/' + id + '.csv')
                 except AttributeError:
                     pass
--- brian's_code/prawtest.py
+++ brian's_code/prawtest.py
@@ -8,27 +8,48 @@ reddit = praw.Reddit(
     client_secret="DEOTHnpheAYiMjAEklkPmXYLT6141A",
     user_agent="prawtest")
 
+def reorder_columns(dataframe, col_name, position):
+    """Reorder a dataframe's column.
+    Args:
+        dataframe (pd.DataFrame): dataframe to use
+        col_name (string): column name to move
+        position (0-indexed position): where to relocate column to
+    Returns:
+        pd.DataFrame: re-assigned dataframe
+    """
+    temp_col = dataframe[col_name]
+    dataframe = dataframe.drop(columns=[col_name])
+    dataframe.insert(loc=position, column=col_name, value=temp_col)
+    return dataframe
+
+
+
 def get_submissions(query, sub, limit=None):
     query2 = query.replace('|','OR')
     query3 = query2.replace('+','AND')
     query4 = query3.replace(' ', '-')
-    filename = f'../data/praw_q={query4}_subreddit={sub}.csv'
+    filename = f'data/praw_q={query4}_subreddit={sub}.csv'
     subreddit = reddit.subreddit(f'{sub}')
     if os.path.exists(filename):
-        pass
+        print('file exists')
     else:
         df = pd.DataFrame([vars(post) for post in subreddit.search(f'{query}', limit = limit) ])
         if 'id' in df.keys():
+            columns_list = ['id', 'author', 'title', 'selftext', 'created','author_flair_text', 'link_flair_text', 'url']
+            for i,j in enumerate(columns_list):
+                df = reorder_columns(dataframe=df, col_name= j, position=i)
+                print('csv created')
+            df.reset_index(drop=True, inplace=True)
             df.to_csv(filename)
         else:
-            pass
+            print('no submissions')
 
 
 if __name__ == '__main__':
     limit = 1000
-    sublist = ['delta8', 'delta8testing']
-    qlist = ['lungs']
+    sublist = ['drugs']
+    qlist = ['delta-8']
 
-    for sub in tqdm(sublist, desc=f'Subreddit = {sub}'):
-        for q in tqdm(qlist, desc = f'Query = {q}'):
+    for sub in tqdm(sublist):
+        for q in tqdm(qlist):
             get_submissions(q, sub, limit=limit)
